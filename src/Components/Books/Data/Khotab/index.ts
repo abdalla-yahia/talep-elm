19886d82ff97c55716be05bd7d662f5ce8af0a77
ابو يحيى عبدالله <abdalla-yahia>
2025-02-06 15:13:42 +0200

    Add Some New Books

--- src/Components/Books/Data/Khotab/index.ts
+++ src/Components/Books/Data/Khotab/index.ts
@@ -2,6 +2,7 @@ import Khotab_1 from './Khotab_1.json';
 import Sameer_Mostafa_1 from './Sameer_Mostafa_1.json';
 import Sameer_Mostafa_2 from './Sameer_Mostafa_2.json';
 import Hewainy_1 from './Hewainy_1.json';
+import Hewainy_2 from './Hewainy_2.json';
 import Khaled_Alrashed_1 from './Khaled_Alrashed_1.json';
 import Mohamed_Hassan from './Mohamed_Hassan.json';
 import Mohamed_Hessine from './Mohamed_Hessine.json';
@@ -15,6 +16,7 @@ export default function Khotab_Books(){
             Sameer_Mostafa_1,
             Sameer_Mostafa_2,
             Hewainy_1,
+            Hewainy_2,
             Khaled_Alrashed_1,
             Mohamed_Hassan,
             Mohamed_Hessine
