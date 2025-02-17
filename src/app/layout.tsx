21371ec264e19e25bd0d2b4df0595633d4b8d777
ابو يحيى عبدالله <abdalla-yahia>
2025-02-17 20:06:36 +0200

    Add Visitor Counter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,24 @@
-import type { Metadata } from "next";
+import type {Metadata} from 'next';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "./globals.css";
-import Header from "@/Components/Header/Header";
-import Footer from "@/Components/Footer/Footer";
-import { Container, Row } from "react-bootstrap";
-import StoreProvider from "./StoreProvider";
-import { ToastContainer } from 'react-toastify';
-import "react-toastify/dist/ReactToastify.css";
-import { Amiri } from "next/font/google";
+import './globals.css';
+import Header from '@/Components/Header/Header';
+import Footer from '@/Components/Footer/Footer';
+import {Container, Row} from 'react-bootstrap';
+import StoreProvider from './StoreProvider';
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {Amiri} from 'next/font/google';
 
 const amiri = Amiri({
-  subsets: ["latin"],
-  weight: "700",
-  display: "swap",
+  subsets: ['latin'],
+  weight: '700',
+  display: 'swap',
   adjustFontFallback: false,
 });
 
 export const metadata: Metadata = {
-  title: "طالب علم شرعي",
-  description: "الموقع عبارة عن مكتبة متكاملة لطالب العلم الشرعي",
+  title: 'طالب علم شرعي',
+  description: 'الموقع عبارة عن مكتبة متكاملة لطالب العلم الشرعي',
 };
 
 export default function RootLayout({
@@ -27,21 +27,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ar" dir="rtl">
-      <body className="bg-gray-400 dark:bg-black text-white">
-      <StoreProvider>
-        <Header />
-        <Container>
-          <Row>
-          <div  className={`${amiri.className} min-h-screen  select-none`}>
-            {children}
-          </div>
-          </Row>
-        </Container>
-        <Footer />
-     </StoreProvider>
-        <ToastContainer  />
+    <html lang='ar' dir='rtl'>
+      <body className='bg-gray-400 dark:bg-black text-white'>
+        <StoreProvider>
+          <Header />
+          <Container>
+            <Row>
+              <div className={`${amiri.className} min-h-screen  select-none`}>{children}</div>
+            </Row>
+          </Container>
+          <Footer />
+        </StoreProvider>
+        <ToastContainer />
       </body>
+      <a href='https://visitorbadge.io/status?path=https%3A%2F%2Ftalep-elm.vercel.app%2F'>
+        <img src='https://api.visitorbadge.io/api/visitors?path=https%3A%2F%2Ftalep-elm.vercel.app%2F&countColor=%23263759' />
+      </a>
     </html>
   );
 }
