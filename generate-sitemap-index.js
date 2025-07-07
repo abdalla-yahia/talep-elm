d0cf36ede55f6c166fcb55780b82d48dbb7fa75a
ابو يحيى عبدالله <abdalla-yahia>
2025-07-07 07:16:20 +0300

    Edit Quran And Radios Url Inside Sitemap

--- generate-sitemap-index.js
+++ generate-sitemap-index.js
@@ -8,8 +8,7 @@ const sitemapIndex = `<?xml version="1.0" encoding="UTF-8"?>
 <sitemapindex 
 xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
->
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
   <sitemap>
     <loc>https://talep-elm.vercel.app/sitemap-0.xml</loc>
     <lastmod>${date}</lastmod>
