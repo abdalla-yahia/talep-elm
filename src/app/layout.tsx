9744361dab047ec226607fcbe1efbeba3742aab4
ابو يحيى عبدالله <abdalla-yahia>
2025-08-03 19:09:18 +0300

    Edit Sitemap File

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -123,14 +123,14 @@ export const metadata: Metadata = {
     address: false,
     email: false,
   },
-  // robots: {
-  //   index: true,
-  //   follow: true,
-  //   nocache: false,
-  //   noimageindex: false,
-  //   noarchive: false,
-  //   nosnippet: false,
-  // },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    noimageindex: false,
+    noarchive: false,
+    nosnippet: false,
+  },
 
 };
 
